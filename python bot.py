d7d892c31d6a1a712df6a0af4b8bed00391e8b91
a70071296-collab <a70071296@gmail.com>
2025-10-05 19:11:37 -0400

    Create python bot.py

---
+++ python bot.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
